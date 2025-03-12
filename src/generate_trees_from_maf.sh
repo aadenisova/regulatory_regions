1412366be84ae53d250d2641c5a1e87d6ff92a66
aadenisova <alexandraa.denisova@gmail.com>
2025-03-12 12:42:21 -0400

    feat: get trees from maf upd

--- src/generate_trees_from_maf.sh
+++ src/generate_trees_from_maf.sh
@@ -30,49 +30,86 @@ tail -n +2 "$input_file" | while read -r line; do
 
     # Выполняем команды для текущей строки
     echo "Обрабатываем ген: $gene"
-    echo "maf_outputs/${seq_id}.maf"
 
-    # mafExtractor
-    mafExtractor --maf "maf_outputs/${seq_id}.maf" \
-                --seq "GCF_003957565.2_bTaeGut1.4.pri.${seq_id}" \
-                --start "${actual_start}" \
-                --stop "${end}" > "maf_extracted/${gene}.maf"
+    # Проверяем, существует ли файл
+    if [ ! -f "maf_extracted/${gene}.maf" ]; then
+        echo "maf_outputs/${seq_id}.maf"
 
-    echo "Ага, мы достали ген: $gene. Oн лежит в maf_extracted/${gene}.maf"
-    # Конвертация в FASTA
-    if python3 src/maf_to_fasta.py "$gene"; then
-        # Скрипт выполнился успешно, продолжаем выполнение
-        echo "Обработка гена $gene"
-        # Здесь можно добавить дополнительные команды
+        # Выполняем mafExtractor только если файла еще нет
+        # mafExtractor
+        mafExtractor --maf "maf_outputs/${seq_id}.maf" \
+                    --seq "GCF_003957565.2_bTaeGut1.4.pri.${seq_id}" \
+                    --start "${actual_start}" \
+                    --stop "${end}" > "maf_extracted/${gene}.maf"
+        echo "Ага, мы достали ген: $gene. Oн лежит в maf_extracted/${gene}.maf"
     else
-        # Скрипт завершился с ошибкой, пропускаем итерацию
-        echo "Пропускаем ген $gene"
-        continue
+        echo "Файл maf_extracted/${gene}.maf уже существует. Пропуск создания."
     fi
 
-    echo "Сконвертировали в fasta: $gene"
+    if [ ! -f "fasta_extracted/${gene}.fa" ]; then
+        echo "Обработка гена $gene"
+        if python3 src/maf_to_fasta.py "$gene"; then
+            # Скрипт выполнился успешно, продолжаем выполнение
+            echo "Сконвертировали в fasta: $gene"
+            # Здесь можно добавить дополнительные команды
+        else
+            # Скрипт завершился с ошибкой, пропускаем итерацию
+            echo "Пропускаем ген $gene"
+            continue
+        fi
+    else
+        echo "fasta_extracted/${gene}.fa уже существует. Пропуск создания."
+    fi
     
-    # Выравнивание с ClustalW2
-    clustalw2 -INFILE="fasta_extracted/${gene}.fa" \
-              -OUTORDER=clustalw2_alignment \
-              -OUTFILE="clustalw2_alignment/${gene}.aln" \
-              -TYPE=DNA \
-              -GAPOPEN=10 \
-              -GAPEXT=0.1 \
-              -PWGAPOPEN=10 \
-              -PWGAPEXT=0.1 \
-              -DNAMATRIX=IUB \
-              -TOSSGAPS
+    if [ ! -f "clustalw2_alignment/${gene}.aln" ]; then
+        # Выравнивание с ClustalW2
+        clustalw2 -INFILE="fasta_extracted/${gene}.fa" \
+                -OUTFILE="clustalw2_alignment/${gene}.aln" \
+                -TYPE=DNA \
+                -GAPOPEN=10 \
+                -GAPEXT=0.1 \
+                -PWGAPOPEN=10 \
+                -PWGAPEXT=0.1 \
+                -DNAMATRIX=IUB \
+                -TOSSGAPS
 
-    echo "Сделали выравнивание: $gene"
+        echo "Сделали выравнивание: $gene"
+    else
+        echo "clustalw2_alignment/${gene}.aln уже существует. Пропуск создания."
+    fi
 
-    result_ids_to_save=$(python3 src/remove_gaps_only_cols.py $gene)
-    python3 src/adjast_tree.py $gene $result_ids_to_save
+    mkdir -p trimed_alignment
 
-    # Построение дерева с IQ-TREE
-    iqtree -s "filtered_alignments/${gene}.aln" \
-           -t "master_trees/${gene}_shaohong_feng.tree" \
-           -redo
+    # export PATH="$PATH:/vggpfs/fs3/vgl/store/adenisova/programs/trimal/source/"
+    trimal -in clustalw2_alignment/${gene}.aln -out trimed_alignment/${gene}.phy -phylip
+
+    # if python3 src/remove_gaps_only_cols.py $gene; then
+    #     # Скрипт выполнился успешно, продолжаем выполнение
+    #     echo "Вторая обработка гена $gene"
+    #     # Здесь можно добавить дополнительные команды
+    # else
+    #     # Скрипт завершился с ошибкой, пропускаем итерацию
+    #     echo "Второй пропуск гена $gene"
+    #     continue
+    # fi
+
+
+    # result_ids_to_save=$(python3 src/remove_gaps_only_cols.py $gene)
+    # python3 src/adjast_tree.py $gene $result_ids_to_save
+
+    # Построение дерева с raxmlHPC
+    mkdir -p raxmlHPC_results
+    # export PATH="$PATH:/vggpfs/fs3/vgl/store/adenisova/programs/standard-RAxML/"
+    raxmlHPC -f e \
+        -s trimed_alignment/${gene}.phy \
+        -t shaohong_feng_for_most_common_species.tree \
+        -n ${gene}.tree \
+        -m GTRGAMMA \
+        -o GCF_900496995.4,GCA_018139145.1
+
+    mv RAxML_result.${gene}.tree raxmlHPC_results
+
+    rm RAxML_rootedTree.${gene}.tree RAxML_log.${gene}.tree RAxML_binaryModelParameters.${gene}.tree RAxML_result.${gene}.tree RAxML_info.${gene}.tree
 
     echo "И даже построили дерево: $gene"
 
